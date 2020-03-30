fc8b35aa4594e91951b764e9eefcea15a69c2a92
Jorge Ernesto Nicho Galagarza <jorge.nicho@utec.edu.pe>
2020-03-30 11:25:28 -0500

    formato

--- main.cpp
+++ main.cpp
@@ -1,57 +1,9 @@
 #include <iostream>
+#import "Shesort.h"
 using namespace std;
-#define MAX 10
-
-class Shesort{
-    int n;
-    int arr[MAX];
-public:
-    void insert_Data();
-    void mostrar_Data();
-    void ordenar_Data();
-};
-
-void Shesort :: insert_Data(){
-    cout<<"Ingresa la cantidad de elementos a ingresar: ";
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-}
-
-void Shesort :: mostrar_Data(){
-    for(int i=1;i<n+1;i++){
-        cout<<arr[i]<<"   ";
-    }
-}
-
-void Shesort :: ordenar_Data(){
-    int part,i, aux;
-    bool band;
-
-    part = n;
-
-    while(part > 1){
-        part = part/2;
-        band = true;
-        while(band){
-            band = false;
-            i=0;
-            while((i+part)<=n){
-                if(arr[i]>arr[i+part]){
-                    aux = arr[i];
-                    arr[i]=arr[i+part];
-                    arr[i+part] = aux;
-                    band = true;
-                }
-                i++;
-            }
-        }
-    }
-}
 
 int main(){
-    cout<<"\n*****Shield Sort*****\n";
+    cout<<"\n*****Shell Sort*****\n";
     Shesort obj{};
     obj.insert_Data();
     obj.ordenar_Data();
