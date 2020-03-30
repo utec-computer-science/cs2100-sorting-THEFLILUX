67c26bea4258d4955b1ac2e1e359ad02f9eb746f
Jorge Ernesto Nicho Galagarza <jorge.nicho@utec.edu.pe>
2020-03-29 20:19:34 -0500

    selection

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define MAX 10
 
-class Inssort{
+class Selsort{
     int n;
     int arr[MAX];
 public:
@@ -11,7 +11,7 @@ public:
     void ordenar_Data();
 };
 
-void Inssort :: insert_Data(){
+void Selsort :: insert_Data(){
     cout<<"Ingresa la cantidad de elementos a ingresar: ";
     cin>>n;
     for(int i=0;i<n;i++){
@@ -19,28 +19,30 @@ void Inssort :: insert_Data(){
     }
 }
 
-void Inssort :: mostrar_Data(){
+void Selsort :: mostrar_Data(){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<"   ";
     }
 }
 
-void Inssort :: ordenar_Data(){
-    int posicion,aux;
-    for(int i=0;i<n;i++){
-        posicion=i;
-        aux=arr[i];
-        while(posicion>0 && arr[posicion-1]>aux){
-            arr[posicion] = arr[posicion-1];
-            posicion--;
+void Selsort :: ordenar_Data(){
+    int min, aux;
+    for (int i = 0; i < n; i++) {
+        min = i;
+        for (int j = i+1; j < n; j++) {
+            if(arr[j] < arr[min]){
+                min = j;
+            }
         }
-        arr[posicion] = aux;
+        aux = arr[i];
+        arr[i] = arr[min];
+        arr[min] = aux;
     }
 }
 
 int main(){
-    cout<<"\n*****Insertion Sort*****\n";
-    Inssort obj{};
+    cout<<"\n*****Selection Sort*****\n";
+    Selsort obj{};
     obj.insert_Data();
     obj.ordenar_Data();
     obj.mostrar_Data();
