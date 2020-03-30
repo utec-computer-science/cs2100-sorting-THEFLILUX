6ab3a9f3974ad2a43e86e0e5beff8fbdfb0c5528
Jorge Ernesto Nicho Galagarza <jorge.nicho@utec.edu.pe>
2020-03-30 12:13:43 -0500

    formato

---
+++ Bubsort.h
@@ -0,0 +1,19 @@
+//
+// Created by theflilux on 30/03/20.
+//
+
+#ifndef CS2100_SORTING_THEFLILUX_BUBSORT_H
+#define CS2100_SORTING_THEFLILUX_BUBSORT_H
+
+#define MAX 10
+
+class Bubsort{
+    int n;
+    int arr[MAX];
+public:
+    void insert_Data();
+    void mostrar_Data();
+    void ordenar_Data();
+};
+
+#endif //CS2100_SORTING_THEFLILUX_BUBSORT_H
--- main.cpp
+++ main.cpp
@@ -1,42 +1,7 @@
 #include <iostream>
+#include "Bubsort.h"
 using namespace std;
-#define MAX 10
 
-class Bubsort{
-    int n;
-    int arr[MAX];
-public:
-    void insert_Data();
-    void mostrar_Data();
-    void ordenar_Data();
-};
-
-void Bubsort :: insert_Data(){
-    cout<<"Ingresa la cantidad de elementos a ingresar: ";
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-}
-
-void Bubsort :: mostrar_Data(){
-    for(int i=1;i<n+1;i++){
-        cout<<arr[i]<<"   ";
-    }
-}
-
-void Bubsort :: ordenar_Data(){
-    int aux;
-    for(int i=0;i<n;i++){
-        for (int j=0;j<n;j++){
-            if (arr[j]>arr[j+1]){
-                aux = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = aux;
-            }
-        }
-    }
-}
 
 int main(){
     cout<<"\n*****Bubble Sort*****\n";
