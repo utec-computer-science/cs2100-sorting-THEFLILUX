280ce0a71f78b6a560af08feaa59719e2f4a83ce
Jorge Ernesto Nicho Galagarza <40152300+THEFLILUX@users.noreply.github.com>
2020-03-30 16:19:09 -0500

    Revert "formato"

--- main.cpp
+++ main.cpp
@@ -6,6 +6,20 @@
 using namespace std;
 
 int main(){
+    cout<<"\n*****Insertion Sort*****\n";
+    Inssort obj1{};
+  
+    obj1.insert_Data();
+    obj1.ordenar_Data();
+    obj1.mostrar_Data();
+  
+    cout<<"\n*****Selection Sort*****\n";
+    Selsort obj2{};
+  
+    obj2.insert_Data();
+    obj2.ordenar_Data();
+    obj2.mostrar_Data();
+  
     cout<<"\n*****Bubble Sort*****\n";
     Bubsort obj3{};
   
