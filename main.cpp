224e11276dd1dfd09f02725802dec975b8d35443
Jorge Ernesto Nicho Galagarza <jorge.nicho@utec.edu.pe>
2020-03-30 16:13:45 -0500

    formato

--- main.cpp
+++ main.cpp
@@ -1,26 +1,11 @@
-
-#include "Inssort.h"
-#include "Selsort.h"
+#include <iostream>
 #include "Bubsort.h"
 using namespace std;
 
 int main(){
-    cout<<"\n*****Insertion Sort*****\n";
-    Inssort obj1{};
-  
-    obj1.insert_Data();
-    obj1.ordenar_Data();
-    obj1.mostrar_Data();
-  
-    cout<<"\n*****Selection Sort*****\n";
-    Selsort obj2{};
-  
-    obj2.insert_Data();
-    obj2.ordenar_Data();
-    obj2.mostrar_Data();
-  
     cout<<"\n*****Bubble Sort*****\n";
     Bubsort obj3{};
+
     obj3.insert_Data();
     obj3.ordenar_Data();
     obj3.mostrar_Data();
