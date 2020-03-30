83189a9aaca619c28d0a3c29d03382e05ae91a40
Jorge Ernesto Nicho Galagarza <jorge.nicho@utec.edu.pe>
2020-03-29 20:28:49 -0500

    quick

--- main.cpp
+++ main.cpp
@@ -1,47 +1,54 @@
 #include <iostream>
 using namespace std;
-#define MAX 10
 
-class Bubsort{
-    int n;
-    int arr[MAX];
+class Quisort{
 public:
-    void insert_Data();
-    void mostrar_Data();
-    void ordenar_Data();
+    static void ordenar_Data(int arr[], int primero, int ultimo);
 };
 
-void Bubsort :: insert_Data(){
-    cout<<"Ingresa la cantidad de elementos a ingresar: ";
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-}
-
-void Bubsort :: mostrar_Data(){
-    for(int i=1;i<n+1;i++){
-        cout<<arr[i]<<"   ";
-    }
-}
-
-void Bubsort :: ordenar_Data(){
-    int aux;
-    for(int i=0;i<n;i++){
-        for (int j=0;j<n;j++){
-            if (arr[j]>arr[j+1]){
-                aux = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = aux;
-            }
+void Quisort :: ordenar_Data(int *arr, int primero, int ultimo){
+    int primeraPos, ultimaPos, central;
+    int pivote;
+    central = (primero + ultimo)/2;
+    pivote = arr[central];
+    primeraPos = primero;
+    ultimaPos = ultimo;
+    do{
+        while(arr[primeraPos] < pivote) primeraPos++;
+        while(arr[ultimaPos] > pivote) ultimaPos--;
+        if(primeraPos<=ultimaPos)
+        {
+            int temp;
+            temp=arr[primeraPos];
+            arr[primeraPos]=arr[ultimaPos];
+            arr[ultimaPos]=temp;
+            primeraPos++;
+            ultimaPos--;
         }
+    }while(primeraPos<=ultimaPos);
+
+    if(primero<ultimaPos){
+        ordenar_Data(arr,primero,ultimaPos);
+    }
+    if(primeraPos<ultimo){
+        ordenar_Data(arr,primeraPos,ultimo);
     }
 }
 
 int main(){
-    cout<<"\n*****Bubble Sort*****\n";
-    Bubsort obj{};
-    obj.insert_Data();
-    obj.ordenar_Data();
-    obj.mostrar_Data();
+    int n;
+    cout<<"\n****Quick Sort*****\n";
+    cout<<"Ingresa la cantidad de elementos a ingresar: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    Quisort obj{};
+    obj.ordenar_Data(arr,0,n-1);
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<"   ";
+    }
 }
