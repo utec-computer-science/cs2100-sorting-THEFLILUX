b1540c2c4df09134189e931be4ac6d44c87f84b0
Jorge Ernesto Nicho Galagarza <jorge.nicho@utec.edu.pe>
2020-03-30 11:36:59 -0500

    formato

--- main.cpp
+++ main.cpp
@@ -1,42 +1,7 @@
 #include <iostream>
+#include "Inssort.h"
 using namespace std;
-#define MAX 10
 
-class Inssort{
-    int n;
-    int arr[MAX];
-public:
-    void insert_Data();
-    void mostrar_Data();
-    void ordenar_Data();
-};
-
-void Inssort :: insert_Data(){
-    cout<<"Ingresa la cantidad de elementos a ingresar: ";
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-}
-
-void Inssort :: mostrar_Data(){
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<"   ";
-    }
-}
-
-void Inssort :: ordenar_Data(){
-    int posicion,aux;
-    for(int i=0;i<n;i++){
-        posicion=i;
-        aux=arr[i];
-        while(posicion>0 && arr[posicion-1]>aux){
-            arr[posicion] = arr[posicion-1];
-            posicion--;
-        }
-        arr[posicion] = aux;
-    }
-}
 
 int main(){
     cout<<"\n*****Insertion Sort*****\n";
