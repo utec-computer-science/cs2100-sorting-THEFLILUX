80c62fa813ad187acf7e7cfde856a2ed6b5a9748
Jorge Ernesto Nicho Galagarza <jorge.nicho@utec.edu.pe>
2020-03-29 20:11:02 -0500

    insertion

---
+++ cmake-build-debug/CMakeFiles/cs2100-sorting-THEFLILUX.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs2100-sorting-THEFLILUX.dir/main.cpp.o"
+  "cs2100-sorting-THEFLILUX"
+  "cs2100-sorting-THEFLILUX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs2100-sorting-THEFLILUX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
